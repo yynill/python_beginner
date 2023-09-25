10156342d7fc70b23f45732549055ccfb37a8ca9
y <145238134+yynill@users.noreply.github.com>
2023-09-25 12:33:41 +0200

    fix balance

--- slot_machine/slot.py
+++ slot_machine/slot.py
@@ -1,13 +1,14 @@
 import random
 # Deposit - Only Numbers Valid
 
-while True:
-    try:
-        deposit = int(input('Deposit amount: $'))
-        print(f'Current Balance: ${deposit} \n')
-        break
-    except ValueError:
-        print('Invalid input. Please enter a valid number.\n')
+
+def get_deposit():
+    while True:
+        try:
+            amount = int(input('Deposit amount: $'))
+            return amount
+        except ValueError:
+            print('Invalid input. Please enter a valid number.\n')
 
 
 def set_slot(lines, bet_size, deposit):
@@ -37,15 +38,17 @@ def set_slot(lines, bet_size, deposit):
             count_winning_rows.append(i)
 
     win = len(count_winning_rows) * (bet_size * 2)
-    loss = bet_size * lines - win
+    loss = bet_size * lines
 
-    new_balance = deposit - loss
+    total = win - loss
 
     print(f'''
 You won: ${win}
-On lines: {count_winning_rows}
-Current Balance: ${new_balance}''')
+On lines: {count_winning_rows}''')
+    return total
 
+
+deposit = get_deposit()
 
 while True:
     menu = input('\nPress Enter to Play || Quit with (q)').lower()
@@ -56,7 +59,8 @@ while True:
         total_bet = lines * bet_size
         print(
             f'Your Bet: {lines} Lines each ${bet_size} ==> Total: ${total_bet}')
-        set_slot(lines, bet_size, deposit)
+        deposit += set_slot(lines, bet_size, deposit)
+        print(f'Current Balance: ${deposit}')
     elif menu == "q":
-        # print(f'\nCashout: {new_balance}')
+        print(f'\nCashout: {deposit}')
         break
